b92c8cdc829343e9dc16330103226570b59e4c2f
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2025-01-15 18:35:13 +0300

    Update rug.rb

--- rug.rb
+++ rug.rb
@@ -1,6 +1,14 @@
 def draw_rug(w, h)
-
-
+  h.times do |i|
+    w.times do |j|
+      if (i+j)%2 == 0
+        print('#')
+      else
+        print('.')
+      end
+    end
+    puts
+  end
 end
 
 draw_rug(600, 800)
